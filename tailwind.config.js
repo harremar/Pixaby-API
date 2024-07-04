92d19ff408bbc69d9f6ea0960d116772d55fc3b9
harremar <78804575+harremar@users.noreply.github.com>
2024-07-04 13:08:10 -0400

    deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust this based on where your files are located
+  ],
   theme: {
     extend: {},
   },
